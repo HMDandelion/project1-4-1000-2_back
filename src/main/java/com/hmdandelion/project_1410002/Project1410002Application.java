54f51acac755ae0f5806ba616d3a687d03028a1e
MIdWit0828 <kindj9897@gmail.com>
2024-05-23 10:04:26 +0900

    최초 커밋

---
+++ src/main/java/com/hmdandelion/project_1410002/Project1410002Application.java
@@ -0,0 +1,13 @@
+package com.hmdandelion.project_1410002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1410002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project1410002Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hmdandelion/project_1410002/Project1410002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmdandelion.project_1410002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1410002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
