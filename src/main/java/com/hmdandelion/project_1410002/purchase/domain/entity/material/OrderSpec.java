447ee263b33ac20d12ea780e3a7e5e72c04554d3
MIdWit0828 <kindj9897@gmail.com>
2024-05-27 11:52:33 +0900

    #9 원자재 엔티티 작성 완료

---
+++ src/main/java/com/hmdandelion/project_1410002/purchase/domain/entity/material/OrderSpec.java
@@ -0,0 +1,26 @@
+package com.hmdandelion.project_1410002.purchase.domain.entity.material;
+
+import com.hmdandelion.project_1410002.inventory.domian.entity.material.MaterialSpec;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "tbl_order_spec")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class OrderSpec {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderSpecCode;
+    private int orderQuantity;
+    private int price;
+    private Long orderCode;
+    @ManyToOne
+    @JoinColumn(name = "spec_code")
+    private MaterialSpec materialSpec;
+}
---
+++ src/main/java/com/hmdandelion/project_1410002/purchase/model/MOrderStatus.java
@@ -0,0 +1,5 @@
+package com.hmdandelion.project_1410002.purchase.model;
+
+public enum MOrderStatus {
+    ORDER_COMPLETED, DELIVERY_SCHEDULED, DELIVERY_COMPLETED, ORDER_END
+}
