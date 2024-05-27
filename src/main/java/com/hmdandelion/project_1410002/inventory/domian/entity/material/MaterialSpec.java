5c9b155dd5eef1246844c06a1fe3baef8ea12dbb
MIdWit0828 <kindj9897@gmail.com>
2024-05-27 11:00:05 +0900

    #9 (05/27) 엔티티 작성중

---
+++ src/main/java/com/hmdandelion/project_1410002/inventory/domian/entity/material/MaterialSpec.java
@@ -0,0 +1,27 @@
+package com.hmdandelion.project_1410002.inventory.domian.entity.material;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "tbl_material_specification")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class MaterialSpec {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long specCode;
+    private String materialName;
+    private String remarks;
+    private String unit;
+    @ManyToOne
+    @JoinColumn(name = "spec_category_code")
+    private Category category;
+    private int safetyStock;
+    private String specification;
+}
--- src/main/java/com/hmdandelion/project_1410002/inventory/domian/entity/material/MaterialStock.java
+++ src/main/java/com/hmdandelion/project_1410002/inventory/domian/entity/material/MaterialStock.java
@@ -1,10 +1,14 @@
 package com.hmdandelion.project_1410002.inventory.domian.entity.material;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hmdandelion.project_1410002.inventory.model.StockDivision;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Table(name = "tbl_material_stock")
 @Entity @Getter
@@ -14,4 +18,21 @@ public class MaterialStock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long stockCode;
+    @Enumerated(value = EnumType.STRING)
+    private StockDivision division;
+    @ManyToOne
+    @JoinColumn(name = "spec_code")
+    private MaterialSpec materialSpec;
+    private int warehouseCode;  //TODO 수정필
+    private int oncomingQuantity;
+    private int actualQuantity;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime storageDatetime;
+    private String remarks;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime inspectionDatetime;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime modificationDatetime;
+    private String modificationReason;
+    private int orderCode; //TODO 수정필
 }
