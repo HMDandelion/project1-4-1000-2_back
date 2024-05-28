dc14cff03f253109c78cc496615d4a39efe13f66
eurdream98 <k01054792361@gmail.com>
2024-05-28 15:46:03 +0900

    refator : repository명 repo로 수정

--- src/main/java/com/hmdandelion/project_1410002/inventory/domian/repository/product/ProductRepo.java
+++ src/main/java/com/hmdandelion/project_1410002/inventory/domian/repository/product/ProductRepo.java
@@ -3,9 +3,8 @@ package com.hmdandelion.project_1410002.inventory.domian.repository.product;
 
 import com.hmdandelion.project_1410002.inventory.domian.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+public interface ProductRepo extends JpaRepository<Product, Long>, ProductRepoCustom {
 }
