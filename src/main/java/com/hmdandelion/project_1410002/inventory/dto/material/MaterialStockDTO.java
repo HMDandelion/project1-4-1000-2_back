d655ef105cc06627be83c780a1368a576ab95580
MIdWit0828 <kindj9897@gmail.com>
2024-05-29 10:19:52 +0900

    #9 (05/29) 머지 전 커밋

--- src/main/java/com/hmdandelion/project_1410002/inventory/dto/material/MaterialStockDTO.java
+++ src/main/java/com/hmdandelion/project_1410002/inventory/dto/material/MaterialStockDTO.java
@@ -1,12 +1,8 @@
 package com.hmdandelion.project_1410002.inventory.dto.material;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter @Setter @ToString
-@RequiredArgsConstructor
 public class MaterialStockDTO {
-    private final Long stockCode;
+
 }
