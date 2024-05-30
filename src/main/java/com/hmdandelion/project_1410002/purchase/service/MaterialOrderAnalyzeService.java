e8a4d158b6e89c88538e19aa0bb87bff5881728b
MIdWit0828 <kindj9897@gmail.com>
2024-05-30 13:19:42 +0900

    #24 (05/30) 하원 전 커밋

--- src/main/java/com/hmdandelion/project_1410002/purchase/service/MaterialOrderAnalyzeService.java
+++ src/main/java/com/hmdandelion/project_1410002/purchase/service/MaterialOrderAnalyzeService.java
@@ -1,20 +1,30 @@
 package com.hmdandelion.project_1410002.purchase.service;
 
+import com.hmdandelion.project_1410002.inventory.domian.entity.product.Bom;
 import com.hmdandelion.project_1410002.inventory.dto.material.dto.MaterialGraphModel;
+import com.hmdandelion.project_1410002.inventory.service.BomService;
 import com.hmdandelion.project_1410002.production.domain.entity.ProductionPlannedList;
+import com.hmdandelion.project_1410002.production.service.ProductionPlannedListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class MaterialOrderAnalyzeService {
+    private final ProductionPlannedListService plannedOrderListService;
+    private final BomService bomService;
 
     public List<MaterialGraphModel> findOrderByMaterialRequirementRatio(Long planCode) {
         //생산계획 리스트를 생산계획 코드로 조회한다 (생산하려는 제품과 그 숫자)
-//        List<ProductionPlannedList> plannedLists =
+        List<ProductionPlannedList> plannedLists = plannedOrderListService.findByPlanCode(planCode);
         //생산하려는 제품의 BOM을 조회한다(생산하려는 제품의 요구수치)
+        Map<Integer, List<Bom>> productCode_BomListMap = new HashMap<>();
+        for ()
+        List<Bom> bomlist = bomService.getBomByProductCode()
         //필요 원자재 정보를 조회한다(원자재의 정보)
         //원자재의 이름과 총 필요량을 산출하여 데이터를 저장한다 called A
         //생산계획 코드로 원자재 주문을 조회한다
