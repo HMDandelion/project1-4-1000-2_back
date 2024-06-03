18db20c4fd22b71be468ec44ca723b7e8942deda
seungjaeshina <seungjaeshin96@gmail.com>
2024-06-03 17:34:56 +0900

    refactor: Line, DefectDetail 엔티티 Status타입 변경, 각 EnumClass 생성

--- src/main/java/com/hmdandelion/project_1410002/production/domain/type/LineStatusType.java
+++ src/main/java/com/hmdandelion/project_1410002/production/domain/type/LineStatusType.java
@@ -3,7 +3,7 @@ package com.hmdandelion.project_1410002.production.domain.type;
 import javax.annotation.processing.Completion;
 
 public enum LineStatusType {
-    IN_PRODUCTION,
-    START_PRODUCTION,
-    PRODUCTION_COMPLETED
+    IN_PRODUCTION, // 작업중
+    START_PRODUCTION, // 작업시작
+    PRODUCTION_START, PRODUCTION_COMPLETED // 작업 완료
 }
