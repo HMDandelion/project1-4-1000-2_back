15ec8dad5e1316c69358b5e1d60ad26b119fb15f
seungjaeshina <seungjaeshin96@gmail.com>
2024-06-04 13:12:08 +0900

    feature: 라인 목록 조회 - 리스트에서 페이지로 변경

--- src/main/java/com/hmdandelion/project_1410002/production/presentation/LineController.java
+++ src/main/java/com/hmdandelion/project_1410002/production/presentation/LineController.java
@@ -9,10 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +34,11 @@ public class LineController {
         final Page<LineResponse> lineResponses = lineService.getLineInfo(lineCode, lineName, lineProduction, lineStatusType);
 
         return ResponseEntity.ok(lineResponses);
+    }
+
+    @PostMapping("/lines")
+    public ResponseEntity<Void> save() {
+
+        return null;
     }
 }
